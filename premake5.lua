84db9ad9de93b81336f8d95dbf507167b77410a1
JohnsonHStephen <johnsonh.stephen@gmail.com>
2023-12-16 16:48:54 -0500

    Fixed Premake for new version

--- premake5.lua
+++ premake5.lua
@@ -48,8 +48,20 @@ project "GLFW"
 
 		files
 		{
+			"src/context.c",
+			"src/init.c",
+			"src/input.c",
+			"src/monitor.c",
+			"src/platform.c",
+			"src/vulkan.c",
+			"src/window.c",
+			"src/null_init.c",
+			"src/null_joystick.c",
+			"src/null_monitor.c",
+			"src/null_window.c",
 			"src/win32_init.c",
 			"src/win32_joystick.c",
+			"src/win32_module.c",
 			"src/win32_monitor.c",
 			"src/win32_time.c",
 			"src/win32_thread.c",
